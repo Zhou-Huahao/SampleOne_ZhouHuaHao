3bb3b7777b29d97c96b59dbf5ea71f9e79f371be
ZhouHuahao <767378041@qq.com>
2019-09-23 11:44:38 +0800

    111

---
+++ src/com/shuangtixi/deptwo/one/car.java
@@ -0,0 +1,24 @@
+package com.shuangtixi.deptwo.one;
+
+ class Vrhicles{
+    private String brand;
+    private String color;
+    public void run(){
+        System.out.println("我已经开动了");
+    }
+    public void showInfo(){
+        System.out.println("商标为"+brand);
+        System.out.println("颜色为"+color);
+}}
+    class car extends Vrhicles{
+    private int seats;
+    public void showCar(){
+      showInfo();
+    }
+ }
+ class Truck extends Vrhicles{
+     private float load;
+     public void showTruck(){
+         showInfo();
+     }
+ }
---
+++ src/com/shuangtixi/deptwo/one/carMean.java
@@ -0,0 +1,8 @@
+package com.shuangtixi.deptwo.one;
+
+public class carMean {
+    public static void main(String[] args) {
+        Vrhicles car=new Vrhicles();
+        car.showInfo();
+    }
+}
